704867cbee4e747f309521bc547be6f18038e7e6
delinqu3nt <toby.bartholovitsch@gmail.com>
2021-01-02 03:13:05 -0800

    Sat Jan  2 03:13:05 AM PST 2021

---
+++ monitor-layout.sh
@@ -0,0 +1,3 @@
+bspc monitor HDMI-A-0 -d I IV
+bspc monitor DisplayPort-1 -d II V
+bspc monitor DisplayPort-2 -d III VI
---
+++ wifi.sh
@@ -0,0 +1,2 @@
+nmcli d wifi list
+yes 733RN801066 | nmcli -a c up TP-Link_CC98
